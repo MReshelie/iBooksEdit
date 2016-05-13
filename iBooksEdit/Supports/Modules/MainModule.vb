c48b6ef851c710cf1da3bc7804cec909f0a57ad5
MReshelieW10 <reshelie2003@mail.ru>
2016-05-13 21:39:37 +0300

    откомпилированно

--- iBooksEdit/Supports/Modules/MainModule.vb
+++ iBooksEdit/Supports/Modules/MainModule.vb
@@ -1,7 +1,9 @@
 ﻿Module MainModule
+    ''' <summary>
+    ''' Гловальные переменные
+    ''' </summary>
     Public pUserF As String
     Public pUserS As String
     Public pUserL As String
     Public pPass As String
-
 End Module
